52e6a61d25353c78e3748e9a736b5839c28397be
Geert Hauwaerts <geert@irssi.org>
2007-07-29 12:55:55 +0000

    .

---
+++ modules/scripts/module.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ *  License disclaimer.
+ *
+ *	For bugreports and other improvements contact The Irssi Project <staff@irssi.org>
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this script; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * Irssi Scripts module.
+ *
+ *    This module will display the Irssi scripts.
+ */
+
+Class IrssiScripts {
+	
+	
+	/*
+	 * Variables to store instances of this module and other core classes.
+         */
+	
+	var $core;
+	
+	
+	/*
+         * Irssi Documentation constructor.
+	 */
+	
+	function IrssiScripts (&$core) {
+		$this->core =& $core;
+	}
+	
+	
+	/*
+	 * Irssi Documentation load() function.
+	 *
+	 *    This function will load the Irssi documentation pages.
+	 */
+	
+	function load () {
+		
+		
+		/*
+		 * Display the correct requested pages.
+		 */
+		
+		if (empty($_REQUEST['action'])) {
+			$_REQUEST['action'] = 'index';
+		}
+		
+		if (file_exists(MODULESDIR . $this->core->CurrentUserModule . DIRECTORY_SEPARATOR . "pages" . DIRECTORY_SEPARATOR . $_REQUEST['action'] . ".page")) {
+			require_once(HTMLDIR . 'header.inc');
+			require_once(MODULESDIR . $this->core->CurrentUserModule . DIRECTORY_SEPARATOR . "pages" . DIRECTORY_SEPARATOR . $_REQUEST['action'] . ".page");
+		} else {
+			header("HTTP/1.0 404 Not Found");
+			
+			require_once(HTMLDIR . 'header.inc');
+			require_once(HTMLDIR . '404.inc');
+		}
+		
+		
+		/*
+		 * The items in the sub-menu.
+		 */
+		
+		$items = array(
+			'scripts' => 'Archive',
+			'faq'     => 'FAQ',
+		);
+		
+		
+		/*
+		 * Display the Irssi footer page.
+		 */
+		
+		require_once(HTMLDIR . 'menu.inc');
+		require_once(HTMLDIR . 'footer.inc');
+	}
+}
+?>
